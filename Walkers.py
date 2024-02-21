d102bf64dc55f7c285a3d10e3636a1cd01892402
jvsgs0 <111323425+jvsgs0@users.noreply.github.com>
2024-02-21 19:45:24 -0300

    Add files via upload

---
+++ Walkers.py
@@ -0,0 +1,34 @@
+import cv2
+
+
+# Crie nosso classificador de corpos
+body_cascade = cv2.CascadeClassifier('C:/Users/User/Desktop/PRO_1-1_C106_TemplateProjeto-main/haarcascade_fullbody.xml')
+
+
+# Inicie a captura de vídeo para o arquivo de vídeo
+cap = cv2.VideoCapture('walking.avi')
+
+# Faça o loop assim que o vídeo for carregado com sucesso
+while True:
+    
+    # Leia o primeiro quadro
+    ret, frame = cap.read()
+
+    # Converta cada quadro em escala de cinza
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Passe o quadro para nosso classificador de corpos
+    body = body_cascade.detectMultiScale(gray, 1.1, 5)
+
+    
+    # Extraia as caixas delimitadoras para quaisquer corpos identificados
+    for(x, y, w, h) in body:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) == 32: #32 é a barra de espaço
+        break
+
+cap.release()
+cv2.destroyAllWindows()
